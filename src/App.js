42c91e046bd2231cb8e88a344f9815ba707365c3
Ethan Collignon <89707534+ethan-collignon@users.noreply.github.com>
2021-10-19 16:14:00 -0400

    additonal components added, linked to serveer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React,{ useState } from 'react';
 import './App.css';
+import Auth from './components/Auth/Auth';
 import Navbar from './components/Navbar/Navbar'
+import Pies from './components/Pies/Pies'
 
 function App() {
 const [sessionToken, setSessionToken] = useState("undefined");
 
   return (
     <div className="App">
-     <h1>This is a test</h1>
-     {sessionToken}
+     {/* <h1>This is a test</h1>
+     {sessionToken} */}
      <Navbar />
+     <Auth/>
+     <Pies/>
     </div>
   );
 }
